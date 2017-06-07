15c0c59a72dbd82732abf19f4bc163c1dcd09357
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-07 09:59:19 -0400

    Add buttons for promotion and deletion - not yet functional

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 const submitForm = document.querySelector('#submitForm')
 const listInfo = document.querySelector('#listInfo')
 
-function deleteItem(){
+function deleteItem(e){
+    e.preventDefault()
+    $(this).parent.remove()
+}
 
+function renderDeleteButton(){
+    const delButton = document.createElement('button')
+    delButton.textContent = 'X'
+
+    return delButton.outerHTML
+}
+
+function renderPromoteButton(){
+    const promote = document.createElement('button')
+    promote.textContent = '+'
+
+    return promote.outerHTML
 }
 
 function renderListItem(value){
     const item = document.createElement('li')
-    item.innerHTML = `${value}`
-    listInfo.prepend(item) //work out how to prepend
+    const del = renderDeleteButton()
+    const promote = renderPromoteButton()
+    item.innerHTML = `${value}    ${promote} ${del}`
+    listInfo.prepend(item)
 }
 
 function handleSubmit(ev){
   ev.preventDefault()
   const f = ev.target
-  renderListItem(f.animals.value)
+  const val = f.animals.value.toUpperCase()
+  renderListItem(val)
 }
 
 submitForm.addEventListener('submit', handleSubmit)
